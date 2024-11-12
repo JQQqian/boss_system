f30dc455ae0cf47a71d05f920a7869e7a36c54fe
happy <291151092@qq.com>
2024-11-12 17:11:52 +0800

    commit

--- vue/src/views/front/ResumeEdit.vue
+++ vue/src/views/front/ResumeEdit.vue
@@ -5,6 +5,7 @@
         在线编辑您的简历
       </div>
       <div style="margin-top: 20px">
+        <pre>{{ data.resumeData}}</pre>
         <el-input style="width: 50%;padding-right: 5px" v-model="data.resumeData.name"
                   placeholder="请输入简历名称"></el-input>
         <el-input style="width: 50%;padding-left: 5px" v-model="data.resumeData.username"
@@ -279,7 +280,8 @@ const loadResume = () => {
   if (data.resumeId) {
     request.get('/resume/selectById/' + data.resumeId).then(res => {
       if (res.code === '200') {
-        data.resumeData = res.data
+        data.resumeData = res.data;
+        console.log(data.resumeData);
       } else {
         ElMessage.error(res.msg)
       }
